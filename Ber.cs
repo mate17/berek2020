52d31d9f8f56e9b96d66be5c365acef886465795
weisz.mate <weisz.mate@28-DIAK14>
2024-11-18 14:55:18 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+    internal class Ber
+    {
+        private string nev;
+        private string neme;
+        private string reszleg;
+        private int belepes;
+        private int bere;
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Neme { get => neme; set => neme = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public int Belepes { get => belepes; set => belepes = value; }
+        public int Bere { get => bere; set => bere = value; }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev} Nem: {Neme} Belépés: {Belepes} Fizetés: {Bere}";
+        }
+
+        public Ber(string sor) 
+        {
+            string[] s = sor.Split(';');
+            Nev = s[0];
+            Neme = s[1];
+            Reszleg = s[2];
+            Belepes = int.Parse(s[3]);
+            Bere = int.Parse(s[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using berek2020;
+using System.Text;
+
+List<Ber>  dolgozo = [];
+
+using StreamReader sr = new("..\\..\\..\\berek2020.txt", Encoding.UTF8);
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    dolgozo.Add(new(sr.ReadLine()));
+}
+Console.Write("3. feladat: ");
+Console.WriteLine($"a dolgozók száma: {dolgozo.Count} fő");
+
+Console.Write("4. feladat: ");
+var f4 = dolgozo.Average(d => d.Bere);
+Console.WriteLine($"Bérek Átlaga: {f4/1000:0.0} eFt");
+
+Console.Write("5. feladat: ");
+Console.Write("Kérem a részleg nevét: ");
+string f5 = Console.ReadLine();
+Console.Write("6. feladat: ");
+int max = 0;
+for (int i = 0; i < dolgozo.Count; i++)
+{
+    if (dolgozo[i].Reszleg  == f5.ToLower())
+    {
+        if (dolgozo[i].Bere > dolgozo[max].Bere)
+        {
+            max = i;
+        }
+    }
+}
+if (max == 0 && dolgozo[max].Reszleg != f5.ToLower())
+{
+    Console.WriteLine("A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    Console.WriteLine($"\n\tNév: {dolgozo[max].Nev}\n\tNem: {dolgozo[max].Neme}\n\tBelépés: {dolgozo[max].Belepes}\n\tBér: {dolgozo[max].Bere} forint");
+}
+Console.WriteLine("7. feladat: Statisztika");
+var f7 = dolgozo.GroupBy(d => d.Reszleg);
+foreach (var fel in f7)
+{
+    Console.WriteLine($"{fel.Key}: {fel.Count()} db");
+}
+Console.WriteLine(f7);
